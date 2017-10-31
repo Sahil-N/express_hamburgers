c83c7d2b944c6995ba20adc3823699ff795fe8b7
Sahil-N <sahilnajeeb1@gmail.com>
2017-10-30 20:41:58 -0700

    yup

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Double Double');
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name, consumed) VALUES ('DoubleStack', true);
+INSERT INTO burgers (name, consumed) VALUES ('Whopper', true);
+INSERT INTO burgers (name, consumed) VALUES ('Rocket Burger', true);
+INSERT INTO burgers (name) VALUES ('Bobs Burger);
